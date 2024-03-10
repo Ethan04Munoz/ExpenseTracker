5a4b07b7c0c3799d22554af80c2fdc1346183cf3
Your Name <you@example.com>
2024-03-09 20:30:22 -0600

    Se pueden añadir categorías de ingresos

--- src/Paginas/AddCategoriaIngresos.jsx
+++ src/Paginas/AddCategoriaIngresos.jsx
@@ -1,19 +1,65 @@
-import React from "react";
-import Navbar from "../componentes/Navbar";
-import Boton from "../componentes/Boton";
+    import React, { useEffect, useState } from "react";
+    import Navbar from "../componentes/Navbar";
+    import Boton from "../componentes/Boton";
+    import { ToastContainer, toast } from 'react-toastify';
+    import 'react-toastify/dist/ReactToastify.css';
 
-function AddCategoriaIngresos(){
-    return (
-        <div className="addCategoriaIngresos">
-            <Navbar enlaceHeader={"/"}/>
-            <form className="formulario" action="">
-                <h1>Añadir una categoria a mis Ingresos</h1>
-                <p>Nombre de la categoria:</p> 
-                <input className="input" type="text" name="" id="" />
-                <Boton contenido="Añadir" clase="Btn BtnBlue"/>
-            </form>
-        </div>
-    )
-}
+    function AddCategoriaIngresos(){
+        const [nuevaCategoria, setNuevaCategoria] = useState('');
+        const [categoriasLS, setCategoriasLS] = useState([]);
+        function obtenerCategoriasLS(){
+            const categoriasLSprov = localStorage.getItem('categoriasIngresos');
+            // Asegurándonos de que categoriasLS siempre sea un array.
+            const categorias = categoriasLSprov ? JSON.parse(categoriasLSprov) : [];
+            setCategoriasLS(categorias);
+            return categorias;
+        }
+        
+        function guardarCategoriaIngresos(e){
+            setNuevaCategoria(e.target.value);
+        }
+        function guardarCategoriaIngresosLS(){
+            let categorias = obtenerCategoriasLS();
+            if(categorias == null || categorias == undefined){
+                categorias = [nuevaCategoria];
+            }else{
+                categorias = JSON.parse(categorias);
+                categorias.push(nuevaCategoria);
+            }
+            categorias = JSON.stringify(categorias);
+            localStorage.setItem('categoriasIngresos', categorias)
+            toast.success('¡La categoría fue añadida con éxito!');
+            setNuevaCategoria('');
+        }
 
-export default AddCategoriaIngresos;
+        useEffect(() => {
+            obtenerCategoriasLS();
+        }, [])
+
+        useEffect(() => {
+            console.log("Categorias LS:", categoriasLS)
+        }, [categoriasLS])
+        return (
+            <div className="addCategoriaIngresos">
+                <Navbar enlaceHeader={"/"}/>
+                <ToastContainer position="bottom-left" />
+                <form className="formulario" action="">
+                    <h1>Añadir una categoria a mis Ingresos</h1>
+                    <p>Nombre de la categoria:</p> 
+                    <input className="input" type="text" name="" id="" placeholder="Ejemplo: Rendimientos portafolio" value={nuevaCategoria} onChange={guardarCategoriaIngresos}/>
+                    <Boton contenido="Añadir" clase="Btn BtnBlue" onClick={guardarCategoriaIngresosLS}/>
+                </form>
+
+                <div className="formulario">
+                    <h2>Tus categorías de Ingresos</h2>
+                    <ul>
+                        {categoriasLS.map((categoria, index) => (
+                            <li key={index}>{categoria}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+
+    export default AddCategoriaIngresos;
