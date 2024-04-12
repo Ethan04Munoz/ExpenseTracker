1be2c94ca2ed061f525d0e4520443be73e1a04ee
Your Name <you@example.com>
2024-04-11 20:02:23 -0600

    Creacion del componente de gestor de atajos de teclado

---
+++ src/componentes/GestorAtajos.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Modal from './Modal.jsx';
+import { useSelector } from 'react-redux';
+import translations from "../redux/translations";
+
+function GestorAtajos() {
+    const [estaCerrado, setEstaCerrado] = useState(false);
+
+    const language = useSelector(state => state.language.language);
+
+    useEffect(() => {
+        const handleEscapeKey = (event) => {
+            console.log("Clic en tecla, ", event, event.key)
+            if (event.key === 'Escape') {
+                setEstaCerrado(true);
+            }
+        };
+
+        document.addEventListener('keydown', handleEscapeKey);
+
+        return () => {
+            document.removeEventListener('keydown', handleEscapeKey);
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log("Esta cerrado: ", estaCerrado)
+    }, [estaCerrado])
+
+    return (
+        <div>
+            {estaCerrado && (
+                <Modal tituloModal={translations[language].configuracion} onClickX={() => setEstaCerrado(false)}/>
+            )}
+        </div>
+    );
+}
+
+export default GestorAtajos;
