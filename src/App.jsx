249eeeb0a3a863dd2b79dc13a0cb8f2d6279204c
Your Name <you@example.com>
2024-04-30 21:27:40 -0600

    Eliminar archivo vacio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {Route, Routes, BrowserRouter} from "react-router-dom";
-import './App.css';
 import Main from "./Paginas/Main";
 import Ingresos from "./Paginas/Ingresos";
 import AddCategoriaIngresos from "./Paginas/AddCategoriaIngresos";
