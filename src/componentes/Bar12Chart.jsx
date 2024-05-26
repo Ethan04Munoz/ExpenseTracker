80418742121bcce1c09fde33a8c0a75911aed0d5
Your Name <you@example.com>
2024-05-25 18:15:52 -0600

    Limitar el tamaño de los graficos de 12 barras

--- src/componentes/Bar12Chart.jsx
+++ src/componentes/Bar12Chart.jsx
@@ -1,54 +1,58 @@
 import React from 'react';
-import { Bar  } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import { generarColorPastelAleatorio } from '../FuncionesGlobales';
 import { mesesEN, mesesES } from '../FuncionesGlobales';
 import { useSelector, useDispatch } from 'react-redux';
 import translations from '../redux/translations.js';
+import './Bar12Chart.css';
 
-const processData = ({data}) => {
-    const language = useSelector(state => state.language.language);
-    console.log("Data: ", data, typeof data)
-    const categorias = [...new Set(data.map(item => item.categoria))];
-    const gastosPorMes = Array(12).fill(null).map(() => ({}));
-  
-    data.forEach(({ cantidad, fecha, categoria }) => {
-      const date = new Date(fecha.split('/').reverse().join('-'));
-      const month = date.getMonth();
-      const gasto = parseFloat(cantidad);
-      if (!gastosPorMes[month][categoria]) {
-        gastosPorMes[month][categoria] = 0;
-      }
-      gastosPorMes[month][categoria] += gasto;
-    });
-  
-    const datasets = categorias.map(categoria => ({
-      label: categoria,
-      data: gastosPorMes.map(gastos => gastos[categoria] || 0),
-      backgroundColor: generarColorPastelAleatorio(),
-    }));
-  
-    return {
-      labels: language == 'es' ? mesesES : mesesEN,
-      datasets,
-    };
+const processData = ({ data }) => {
+  const language = useSelector(state => state.language.language);
+  console.log("Data: ", data, typeof data)
+  const categorias = [...new Set(data.map(item => item.categoria))];
+  const gastosPorMes = Array(12).fill(null).map(() => ({}));
+
+  data.forEach(({ cantidad, fecha, categoria }) => {
+    const date = new Date(fecha.split('/').reverse().join('-'));
+    const month = date.getMonth();
+    const gasto = parseFloat(cantidad);
+    if (!gastosPorMes[month][categoria]) {
+      gastosPorMes[month][categoria] = 0;
+    }
+    gastosPorMes[month][categoria] += gasto;
+  });
+
+  const datasets = categorias.map(categoria => ({
+    label: categoria,
+    data: gastosPorMes.map(gastos => gastos[categoria] || 0),
+    backgroundColor: generarColorPastelAleatorio(),
+  }));
+
+  return {
+    labels: language == 'es' ? mesesES : mesesEN,
+    datasets,
+  };
 };
 
-function Bar12Chart(data){
-    const chartData = processData(data);
+function Bar12Chart(data) {
+  const chartData = processData(data);
 
-    return (
-      <Bar 
-        data={chartData} 
-        options={{ 
-          plugins: { legend: { position: 'top' } }, 
-          scales: { 
-            x: { stacked: true }, 
-            y: { stacked: true } 
-          } 
-        }} 
+  return (
+    <div className="contenedor12Bar">
+      <Bar
+        data={chartData}
+        options={{
+          plugins: { legend: { position: 'top' } },
+          scales: {
+            x: { stacked: true },
+            y: { stacked: true }
+          }
+        }}
       />
-    );
+    </div>
+
+  );
 }
 
 export default Bar12Chart;
